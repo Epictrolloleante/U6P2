93d723c2352df9cd61d4008700344965977679e0
reyes <reyes@DESKTOP-EPIC>
2021-12-16 01:47:16 -0600

    Agregar archivos de proyecto.

---
+++ U6P2/ConexionMysql.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace U6P2
+{
+    internal class ConexionMysql
+    {
+        string usuario = "postgres",
+            contraseña = "root",
+            host = "localhost",
+            port = "5432",
+            basedatos = "";
+        NpgsqlConnection con;
+
+        public ConexionMysql(string bd)
+        {
+            basedatos = bd;
+        }
+
+        public string getBasedatos()
+        {
+            return basedatos;
+        }
+
+        public void conectar()
+        {
+            try
+            {
+                Debug.WriteLine("Conexion a la BD");
+                string cadenaConexion = "Server=" + host + ";Port=" + port + ";User Id=" + usuario
+                    + ";Password=" + contraseña + ";Database=" + basedatos + ";";
+                con = new NpgsqlConnection(cadenaConexion);
+                con.Open();
+                Debug.WriteLine("Conexion exitosa");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Ha ocurrido la sig excepcion: " + e.Message);
+                con.Close();
+            }
+
+        }
+
+        public void desconectar()
+        {
+            try
+            {
+                con.Close();
+                Debug.WriteLine("Desconectado de la BD");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Ha ocurrido la sig excepcion: " + ex.Message);
+            }
+        }
+
+        public DataTable consultar(string tabla)
+        {
+            try
+            {
+                string query = "SELECT * FROM \"" + tabla + "\" ;";
+                NpgsqlCommand conector = new NpgsqlCommand(query, con);
+                NpgsqlDataAdapter datos = new NpgsqlDataAdapter(conector);
+                DataTable tablad = new DataTable();
+                datos.Fill(tablad);
+                return tablad;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Ha ocurrido la sig excepcion: " + ex.Message);
+                return null;
+            }
+        }
+
+        public DataTable construirComboBox()
+        {
+            try
+            {
+                string query = "SELECT tablename FROM pg_tables WHERE schemaname = 'public';";
+                NpgsqlCommand conector = new NpgsqlCommand(query, con);
+                NpgsqlDataAdapter datos = new NpgsqlDataAdapter(conector);
+                DataTable tablad = new DataTable();
+                
+                datos.Fill(tablad);
+                return tablad;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Ha ocurrido la sig excepcion: " + ex.Message);
+                return null;
+            }
+
+        }
+        /*
+        public ResultSet consultaIndividual(string tabla, string id)
+        {
+            try
+            {
+                ResultSet rs;
+                Statement stmt = (Statement)con.createStatement();
+                string query = "SELECT * FROM " + tabla + " WHERE " + id + ";";
+                rs = stmt.executeQuery(query);
+                return rs;
+            }
+            catch (SQLException ex)
+            {
+                Logger.getLogger(ConexionMysql.class.getName()).log(Level.SEVERE, null, ex);
+        return null;
+                }
+            }
+
+
+                public bool insertar(string tabla, string values)
+                {
+                    try
+                    {
+                        Statement stmt = (Statement)con.createStatement();
+                        string query = "INSERT INTO " + tabla + " VALUES(" + values + ");";
+                        stmt.executeUpdate(query);
+                        Debug.WriteLine("Insercion Exitosa");
+                        return true;
+                    }
+                    catch (SQLException ex)
+                    {
+                        Logger.getLogger(ConexionMysql.class.getName()).log(Level.SEVERE, null, ex);
+        return false;
+                }
+        }
+
+        public boolean modificar(string tabla, string values)
+        {
+            try
+            {
+                Statement stmt = (Statement)con.createStatement();
+                string query = "UPDATE " + tabla + " SET " + values + ";";
+                stmt.executeUpdate(query);
+                return true;
+            }
+            catch (SQLException ex)
+            {
+                Logger.getLogger(ConexionMysql.class.getName()).log(Level.SEVERE, null, ex);
+        return false;
+                }
+            }
+
+            public boolean eliminar(string tabla, string condicion)
+        {
+            try
+            {
+                Statement stmt = (Statement)con.createStatement();
+                string query = "DELETE FROM " + tabla + " WHERE " + condicion + ";";
+                stmt.executeUpdate(query);
+                return true;
+            }
+            catch (SQLException ex)
+            {
+                Logger.getLogger(ConexionMysql.class.getName()).log(Level.SEVERE, null, ex);
+        return false;
+                }
+            }
+
+
+
+
+
+
+        */
+
+
+    }
+}
---
+++ U6P2/ConsultaFrame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U6P2
+{
+    public partial class ConsultaFrame : Form
+    {
+        ConexionMysql mysql = new ConexionMysql("obras");
+        public ConsultaFrame()
+        {
+            InitializeComponent();
+            inicializar();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mi rodilla shonen");
+        }
+
+        public void inicializar() {
+            mysql.conectar();
+            comboBox1.DataSource = mysql.construirComboBox();
+            comboBox1.DisplayMember = "tablename";
+            comboBox1.ValueMember = "tablename";
+            string s= comboBox1.SelectedValue+"";
+            dataGridView1.DataSource = mysql.consultar(s);
+            label1.Text = "BASE DE DATOS: \"" + mysql.getBasedatos()+"\"";
+            mysql.desconectar();
+            
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mysql.conectar();
+            string s = comboBox1.SelectedValue + "";
+            dataGridView1.DataSource = mysql.consultar(s);
+            mysql.desconectar();
+        }
+    }
+}
